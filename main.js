740e79c6028679e5fe055e9c13b9ccd001e68fe2
Tahar Meijs <tntmeijs@gmail.com>
2023-01-07 18:01:12 +0100

    feat: add coming soon page

---
+++ main.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    colors: {
+      red: "#EB455F",
+      white: "#FCFFE7",
+      blue: "#BAD7E9",
+      navy: "#2B3467",
+    },
+    backgroundImage: {
+      lines: "url('./assets/lines.png')",
+    },
+  },
+};
